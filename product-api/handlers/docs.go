cf863f2deba6172968b77ae6031ffcea0352e892
Baby Lin <58794134+HongbinLin1995@users.noreply.github.com>
2022-03-31 15:05:39 -0700

    udpate

--- product-api/handlers/docs.go
+++ product-api/handlers/docs.go
@@ -15,7 +15,7 @@
 // swagger:meta
 package handlers
 
-import "github.com/HongbinLin1995/go-microservice/data"
+import "github.com/HongbinLin1995/go-microservice/product-api/data"
 
 //
 // NOTE: Types defined here are purely for documentation purposes
